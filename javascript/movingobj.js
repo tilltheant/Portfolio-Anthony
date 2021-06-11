a679f4125e2b1ad0724423a18ce4641b445207ab
Anthony de la fuente <tilltheant@hotmail.com>
2021-06-12 01:19:31 +0200

    added pages and costimization

--- javascript/movingobj.js
+++ javascript/movingobj.js
@@ -1,119 +1,30 @@
-function RandomObjectMover(obj, container) {
-	this.$object = obj;
-  this.$container = container;
-  this.container_is_window = container === window;
-  this.pixels_per_second = 250;
-  this.current_position = { x: 0, y: 0 };
-  this.is_running = false;
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-// Set the speed of movement in Pixels per Second.
-RandomObjectMover.prototype.setSpeed = function(pxPerSec) {
-	this.pixels_per_second = pxPerSec;
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-RandomObjectMover.prototype._getContainerDimensions = function() {
-   if (this.$container === window) {
-       return { 'height' : this.$container.innerHeight, 'width' : this.$container.innerWidth };
-   } else {
-   	   return { 'height' : this.$container.clientHeight, 'width' : this.$container.clientWidth };
-   }
-}
-
-RandomObjectMover.prototype._generateNewPosition = function() {
-
-	// Get container dimensions minus div size
-  var containerSize = this._getContainerDimensions();
-	var availableHeight = containerSize.height - this.$object.clientHeight;
-  var availableWidth = containerSize.width - this.$object.clientHeight;
-    
-  // Pick a random place in the space
-  var y = Math.floor(Math.random() * availableHeight);
-  var x = Math.floor(Math.random() * availableWidth);
-    
-  return { x: x, y: y };    
-}
-
-RandomObjectMover.prototype._calcDelta = function(a, b) {
-	var dx   = a.x - b.x;         
-  var dy   = a.y - b.y;         
-  var dist = Math.sqrt( dx*dx + dy*dy ); 
-  return dist;
-}
-
-RandomObjectMover.prototype._moveOnce = function() {
-		// Pick a new spot on the page
-    var next = this._generateNewPosition();
-    
-    // How far do we have to move?
-    var delta = this._calcDelta(this.current_position, next);
-    
-		// Speed of this transition, rounded to 2DP
-		var speed = Math.round((delta / this.pixels_per_second) * 100) / 100;
-    
-    //console.log(this.current_position, next, delta, speed);
-          
-    this.$object.style.transition='transform '+speed+'s linear';
-    this.$object.style.transform='translate3d('+next.x+'px, '+next.y+'px, 0)';
-    
-    // Save this new position ready for the next call.
-    this.current_position = next;
-  
-};
-
-RandomObjectMover.prototype.start = function() {
-
-	if (this.is_running) {
-  	return;
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-
-	// Make sure our object has the right css set
-  this.$object.willChange = 'transform';
-  this.$object.pointerEvents = 'auto';
-	
-  this.boundEvent = this._moveOnce.bind(this)
-  
-  // Bind callback to keep things moving
-  this.$object.addEventListener('transitionend', this.boundEvent);
-  
-  // Start it moving
-  this._moveOnce();
-  
-  this.is_running = true;
-}
-
-RandomObjectMover.prototype.stop = function() {
-
-	if (!this.is_running) {
-  	return;
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  
-  this.$object.removeEventListener('transitionend', this.boundEvent);
-  
-	this.is_running = false;
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-
-// Init it
-var x = new RandomObjectMover(document.getElementById('a'), window);
-
-
-// Toolbar stuff
-document.getElementById('start').addEventListener('click', function(){
-	x.start();
-});
-document.getElementById('stop').addEventListener('click', function(){
-	x.stop();
-});
-document.getElementById('speed').addEventListener('keyup', function(){
-  if (parseInt(this.value) > 3000 ) {
- 		alert('Don\'t be stupid, stupid');
-    this.value = 250;
-  }
-	x.setSpeed(parseInt(this.value));
-});
-
-
-// Start it off
-
-x.start();
